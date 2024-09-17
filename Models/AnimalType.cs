c6277b5a9896040873731ded0da02d88524db6b9
Tribog <julix4212@gmail.com>
2024-09-17 07:05:40 -0500

    feat: the class "AnimalType" was configured for later creation in tha database

--- Models/AnimalType.cs
+++ Models/AnimalType.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API_Farm.Models;
 
+[Table("animal_type")]
 public class AnimalType
 {
+    [Column("id")]
     public int Id { get; set; }
+    [Column("name")]
     public required string Name { get; set; } // "Mammal", "birds", "fishes", "reptails"
+    [Column("descrition")]
     public string? Description { get; set; } //"They are the animal that drink milk from..."
 
 }
