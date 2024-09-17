ed235f48dab4813f1c1a539b99d3e324d1af0283
Tribog <julix4212@gmail.com>
2024-09-17 08:55:09 -0500

    feat: the endpoint for search data by it id were created

--- Controllers/V1/AnimalTypes/AnimalTypesController.cs
+++ Controllers/V1/AnimalTypes/AnimalTypesController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API_Farm.Data;
+using API_Farm.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace API_Farm.Controllers.V1.AnimalTypes;
 [ApiController]
@@ -19,6 +21,13 @@ public class AnimalTypesController : ControllerBase
     }
 
     [HttpGet]
+    [SwaggerOperation(
+        Summary = "Retrieves all animal types",
+        Description = "Gets a list of all animal types in the database."
+    )]
+    [SwaggerResponse(200, "Returns a list of animal types.", typeof(IEnumerable<AnimalType>))]
+    [SwaggerResponse(204, "There are no registered animal types.")]
+    [SwaggerResponse(500, "An internal server error occurred.")]
     public async Task<IActionResult> GetAll()
     {
         var animalTypes = await Context.AnimalTypes.ToListAsync();
@@ -29,6 +38,33 @@ public class AnimalTypesController : ControllerBase
         }
         return Ok(animalTypes);
     }
+    [HttpGet]
+    public async Task<IActionResult> GetById([FromRoute] int id)
+    {
+        var animalType = await Context.AnimalTypes.FindAsync(id);
+        if(animalType == null)
+        {
+            return NoContent();
+        }
+        return Ok(animalType);
+     }
+
+    [HttpPost]
+    public async Task<IActionResult> create(AnimalType nuevoAnimalType)
+    {
+        if(ModelState.IsValid == false)
+        {
+            return BadRequest(ModelState);
+        }
+        Context.AnimalTypes.Add(nuevoAnimalType);
+        await Context.SaveChangesAsync();
+        return Ok("Se creo el tipo de animal");
+    }
+
+
+
+
+
 
 }
 
--- Models/AnimalType.cs
+++ Models/AnimalType.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,14 @@ namespace API_Farm.Models;
 public class AnimalType
 {
     [Column("id")]
+    [Key]
     public int Id { get; set; }
     [Column("name")]
+    [MinLength(4, ErrorMessage ="The field only accept 4 letters at minimun.")]
     public required string Name { get; set; } // "Mammal", "birds", "fishes", "reptails"
+
     [Column("descrition")]
+    [MaxLength(500, ErrorMessage = "The field only accept 500 letters at maximun")]
     public string? Description { get; set; } //"They are the animal that drink milk from..."
 
 }
