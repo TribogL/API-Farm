3cbb34fa05100153a8afcdadb7352be847ea804a
Tribog <julix4212@gmail.com>
2024-09-18 07:47:49 -0500

    feat: seed data added to "animal_types" table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API_Farm.Models;
+using API_Farm.Seeders;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Internal;
 
@@ -13,4 +14,10 @@ public class ApplicationDbContext : DbContext
     public DbSet<AnimalType> AnimalTypes { get; set; }
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        AnimalTypeSeeder.Seed(modelBuilder);
+    }
 }
